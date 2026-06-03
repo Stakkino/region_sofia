e11773d51ece18e9a39fe91c081d59133a425faa
Stakkino <jhenstakkino@gmail.com>
2026-06-03 19:33:30 +0300

    backend terminer - ok

--- back/cfg/settings.py
+++ back/cfg/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,6 +84,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'cfg.wsgi.application'
 
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
@@ -89,11 +92,11 @@ WSGI_APPLICATION = 'cfg.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sofia_db',
-        'USER': 'postgres',
-        'PASSWORD': 'stakkino',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
